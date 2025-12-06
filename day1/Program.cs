ad3429d2def59bc87132cfb7644d8f9262c56882
fudgebucket27 <fudgebucket27@gmail.com>
2025-12-06 11:45:40 +1000

    initial commit

---
+++ day1/Program.cs
@@ -0,0 +1,43 @@
+﻿var input = File.ReadAllLines("input.txt");
+var dialPosition = 50;
+var password = 0;
+foreach(var line in input)
+{
+    //Get rotation info
+    var rotationDirection = line[0];
+    var rotationAmount = Int32.Parse(line.Substring(1,line.Length-1));
+    //Console.WriteLine($"Turning {rotationDirection} direction, {rotationAmount} amount");
+
+    for(int i = 1; i <= rotationAmount; i++)
+    {
+        //Console.WriteLine($"Dial {dialPosition}");
+        if(rotationDirection == 'L')
+        {
+            if(dialPosition == 0)
+            {
+                dialPosition = 99;
+ 
+            }
+            else
+            {
+                dialPosition = dialPosition - 1; 
+            }
+        }
+        else
+        {
+            if(dialPosition == 99)
+            {
+                dialPosition = 0;
+            }
+            else
+            {
+                dialPosition = dialPosition + 1; 
+            }
+        }
+    }
+    if(dialPosition == 0)
+   {
+      password++;
+    }
+}
+Console.WriteLine($"Password {password}");
